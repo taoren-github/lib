73ab56473d970f7c36f7f15f3d291a79c8d87b9b
Tao Ren <taoren@fb.com>
2018-04-16 15:39:16 -0700

    add list insertion functions

--- c/linklist.h
+++ c/linklist.h
@@ -6,4 +6,27 @@ struct link_list {
 	struct link_list *next;
 };
 
+static inline void
+__list_insert(struct link_list *node,
+              struct link_list *prev,
+              struct link_list *next)
+{
+	node->prev = prev;
+	node->next = next;
+	prev->next = node;
+	next->prev = node;
+}
+
+static inline void
+list_add_before(struct link_list *node, struct link_list *ref)
+{
+	__list_insert(node, ref->prev, ref);
+}
+
+static inline void
+list_add_after(struct link_list *node, struct link_list *ref)
+{
+	__list_insert(node, ref, ref->next);
+}
+
 #endif /* __LINK_LIST_H__ */
